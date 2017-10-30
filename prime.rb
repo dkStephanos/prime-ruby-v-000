5449917978cf1f81e150807675ad26804749e5b9
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-30 17:14:21 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def prime?(number)
-  if number * -1 == 1 || number * -1 == 2
+  if number * -1 == 1 || number * -1 == 2 || number == 0
     return false
   end
   (2..(number.abs - 1)).each do |num|
