0180a25dd1198e18a5a8840fe1fb377dbb73f055
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-30 17:09:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,8 @@
-require 'pry'
-
 def prime?(number)
 
   (2..(number.abs - 1)).each do |num|
-    binding.pry
-    if number.abs % num != 0
+    if(number.abs % num != 0)
       return false
-      binding.pry
     end
   end
   true
